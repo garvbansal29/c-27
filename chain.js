7359626e5bd6e9f4d4eea861f2177192d8a22920
garvbansal29 <garv.bansal4@outlook.com>
2020-08-25 20:53:01 +0530

    we have added two bodies

---
+++ chain.js
@@ -0,0 +1,16 @@
+class Chain{
+    constructor(bodyA,bodyB){
+        var options = {
+     bodyA:bodyA,
+     bodyB:bodyB,
+     length:50,
+     stiffness:1
+        }
+        this.Chain = Constraint.create(options);
+        World.add(world,this.Chain);
+    }
+    display(){
+       strokeWeight(3);
+       line(this.Chain.bodyA.position.x,this.Chain.bodyA.position.y,this.Chain.bodyB.position.x,this.Chain.bodyB.position.y); 
+    }
+}
